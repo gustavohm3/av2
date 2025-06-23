5832574c3e86eab866c8a7ed301640d5482e9380
Gustavo Henrique <126521748+gustavohm3@users.noreply.github.com>
2025-06-23 17:07:28 -0300

    Add files via upload

---
+++ src/api/products.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://leoproti.com.br:8004'
+});
+
+export const getProducts = () => api.get('/produtos');
+export const createProduct = data => api.post('/produtos', data);
+export const updateProduct = (id, data) => api.put(`/produtos/${id}`, data);
+export const deleteProduct = id => api.delete(`/produtos/${id}`);
